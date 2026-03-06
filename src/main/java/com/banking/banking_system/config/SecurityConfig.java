3ca96d76050dfd64f863573991b7cde93be04162
Ayush Pandey <ayushpandeyff1212@gmail.com>
2026-03-07 01:28:45 +0530

    added swagger cors

--- src/main/java/com/banking/banking_system/config/SecurityConfig.java
+++ src/main/java/com/banking/banking_system/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors(cors->{})
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
