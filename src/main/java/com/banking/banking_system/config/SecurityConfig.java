a13063bbcc150cf0c91c48dcc7380d5c65ab3f89
Ayush Pandey <ayushpandeyff1212@gmail.com>
2026-03-06 18:41:41 +0530

    configure for railway deployment

--- src/main/java/com/banking/banking_system/config/SecurityConfig.java
+++ src/main/java/com/banking/banking_system/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)throws Exception{
         http.csrf(csrf->csrf.disable()).authorizeHttpRequests(auth->auth
-                .requestMatchers("/api/users/register","/api/users/login").permitAll()
+                .requestMatchers("/","/api/users/register","/api/users/login").permitAll()
                         .requestMatchers("/api/wallet/**")
                 .authenticated().anyRequest().authenticated())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
---
+++ src/main/java/com/banking/banking_system/controller/testController.java
@@ -0,0 +1,12 @@
+package com.banking.banking_system.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @GetMapping("/")
+    public String test(){
+        return "app is running";
+    }
+}
